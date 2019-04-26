6d992db4a4bb723b6d1fe582ea1f31fc0355a7e3
Julien Phalip <jphalip@gmail.com>
2019-04-26 11:23:57 -0700

    Add all 4 delegation token endpoints to load tests

--- load-testing/client.py
+++ load-testing/client.py
@@ -9,8 +9,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from base64 import b64encode, b64decode
 import time
+import tempfile
+import shutil
 
 import grpc
 import gssapi
@@ -25,12 +28,29 @@ BROKER_REALM = 'BROKER'
 
 class BrokerClient:
 
-    def __init__(self, host, credentials, certificate):
-        self.host = host
-        self.credentials = credentials
-        self.certificate = certificate
+    def kinit(self, principal, password):
+        # Create credentials cache in temporary directory
+        self.cache_dir = tempfile.mkdtemp()
+        ccache = 'FILE:{}/ccache'.format(self.cache_dir)
+        os.environ['KRB5CCNAME'] = ccache
+        store = {b'ccache': ccache.encode('UTF-8')}
+        # Acquire new credentials
+        name = gssapi.Name(principal, gssapi.NameType.kerberos_principal)
+        acquire_credentials = gssapi.raw.acquire_cred_with_password(name, password.encode('ascii'))
+        self.credentials = acquire_credentials.creds
+        # Store credentials in the cache
+        gssapi.raw.store_cred_into(store, self.credentials, usage='initiate', overwrite=True)
 
-    def get_metadata(self):
+    def kdestroy(self):
+        self.credentials = None
+        shutil.rmtree(self.cache_dir)
+
+    def __init__(self, host, certificate):
+        self.host = host
+        self.certificate = certificate
+        self.credentials = None
+
+    def get_spnego_token(self):
         """
         Obtain a SPNEGO token for the broker service and set the token to
         the 'authorization' metadata header.
@@ -42,15 +62,21 @@ class BrokerClient:
             name=service_name, mech=spnego_mech_oid, usage='initiate', creds=self.credentials)
         response = context.step()
         spnego_token = b64encode(response).decode()
-        metadata = [('authorization', 'Negotiate {}'.format(spnego_token))]
+        return spnego_token
+
+    def get_metadata(self, session_token=None):
+        if session_token is not None:
+            metadata = [('authorization', 'BrokerSession {}'.format(session_token))]
+        else:
+            metadata = [('authorization', 'Negotiate {}'.format(self.get_spnego_token()))]
         return metadata
 
     def get_stub(self):
-        credentials = grpc.ssl_channel_credentials(self.certificate)
-        channel = grpc.secure_channel('{}:{}'.format(self.host, BROKER_PORT), credentials)
+        ssl_credentials = grpc.ssl_channel_credentials(self.certificate)
+        channel = grpc.secure_channel('{}:{}'.format(self.host, BROKER_PORT), ssl_credentials)
         return BrokerStub(channel)
 
-    def call_endpoint(self, endpoint, parameters=None):
+    def call_endpoint(self, endpoint, parameters=None, session_token=None):
         start_time = time.time()
         try:
             stub = self.get_stub()
@@ -65,7 +91,7 @@ class BrokerClient:
                     setattr(request, key, value)
 
             # Set SPNEGO token in the metadata
-            metadata = self.get_metadata()
+            metadata = self.get_metadata(session_token)
 
             # Call the gRPC endpoint
             endpoint_func = getattr(stub, endpoint)
--- load-testing/locustfile.py
+++ load-testing/locustfile.py
@@ -9,35 +9,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import time
-import tempfile
 import subprocess
 
 import gssapi
-from locust import Locust, TaskSet, events, task
+from locust import Locust, TaskSequence, task, seq_task
 
 from client import BrokerClient
 from settings import TEST_USERS, REALM
 
 SCOPE = 'https://www.googleapis.com/auth/devstorage.read_write'
 BROKER_HOST = '10.2.1.255.xip.io'
+USER = TEST_USERS[0]
+USER_FULL = '{}@{}'.format(USER, REALM)
 
-
-# Create credentials cache in temporary directory
-TMPDIR = tempfile.mkdtemp()
-CCACHE = 'FILE:{}/ccache'.format(TMPDIR)
-os.environ['KRB5CCNAME'] = CCACHE
-STORE = {b'ccache': CCACHE.encode('UTF-8')}
-
-def login(principal, password):
-    # Acquire new credentials
-    name = gssapi.Name(principal, gssapi.NameType.kerberos_principal)
-    acquire_credentials = gssapi.raw.acquire_cred_with_password(name, password)
-    credentials = acquire_credentials.creds
-    # Store credentials in the cache
-    gssapi.raw.store_cred_into(STORE, credentials, usage='initiate', overwrite=True)
-    return credentials
 
 def get_certificate():
     # Retrieve the TLS certificate from the VM metadata
@@ -48,35 +32,52 @@ def get_certificate():
     certificate, _ = out.communicate()
     return certificate
 
-# Acquire credentatials for a user
-USER = TEST_USERS[0]
-USER_FULL = '{}@{}'.format(USER, REALM)
-CREDENTIALS = login(USER_FULL, USER.encode('ascii'))
-CERTIFICATE = get_certificate()
 
+class JobSimulation(TaskSequence):
 
-class UserBehavior(TaskSet):
+    def on_start(self):
+        self.client.kinit(USER_FULL, USER)
 
-    @task(1)
+    def on_stop(self):
+        self.client.kdestroy()
+
+    @seq_task(1)
     def get_session_token(self):
-        self.client.call_endpoint(
+        response = self.client.call_endpoint(
             'GetSessionToken',
-            dict(owner=USER_FULL, scope=SCOPE)
+            dict(owner=USER_FULL, scope=SCOPE, renewer=USER_FULL)
+        )
+        self.session_token = response.session_token
+
+    @seq_task(2)
+    def renew_session_token(self):
+        self.client.call_endpoint(
+            'RenewSessionToken',
+            dict(session_token=self.session_token),
         )
 
-    @task(20)
+    @seq_task(3)
+    @task(100)
     def get_access_token(self):
         self.client.call_endpoint(
             'GetAccessToken',
-            dict(owner=USER_FULL, scope=SCOPE)
+            dict(owner=USER_FULL, scope=SCOPE),
+            self.session_token
+        )
+
+    @seq_task(4)
+    def cancel_session_token(self):
+        self.client.call_endpoint(
+            'CancelSessionToken',
+            dict(session_token=self.session_token),
         )
 
 
 class BrokerUser(Locust):
     host = BROKER_HOST
-    task_set = UserBehavior
+    task_set = JobSimulation
     min_wait = 0
     max_wait = 1
 
     def __init__(self, *args, **kwargs):
-        self.client = BrokerClient(self.host, CREDENTIALS, CERTIFICATE)
+        self.client = BrokerClient(self.host, get_certificate())
