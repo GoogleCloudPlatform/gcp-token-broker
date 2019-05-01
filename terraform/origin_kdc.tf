b8191ee6e5bfc54bcf5c277d8b370928f784b109
Julien Phalip <jphalip@gmail.com>
2019-04-30 18:18:17 -0700

    Fix origin KDC startup script

--- terraform/origin_kdc.tf
+++ terraform/origin_kdc.tf
@@ -100,7 +100,7 @@ data "template_file" "startup_script_origin_kdc" {
         kadmin.local -q "addprinc -pw ${var.test_users[2]} ${var.test_users[2]}"
 
         # Create broker principal and keytab
-        kadmin.local -q "addprinc broker/${var.broker_service_hostname}"
+        kadmin.local -q "addprinc -randkey broker/${var.broker_service_hostname}"
         kadmin.local -q "ktadd -k /etc/broker.keytab broker/${var.broker_service_hostname}"
     EOT
   }
